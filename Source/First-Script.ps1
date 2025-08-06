d519915e06101d78b4938dbcef13c3fce5d84f0a
Eimer Peralta <113135668+eimerperalta@users.noreply.github.com>
2025-08-06 00:30:53 -0400

    Add initial PowerShell script to the Source directory

---
+++ Source/First-Script.ps1
@@ -0,0 +1,4 @@
+# This is a test PowerShell script
+# It is located in the testRepo-ep-01 repository under the Source directory
+Write-Host "Hello, this is the First-Script.ps1 file in the testRepo-ep-01 repository." -ForegroundColor Green
+# You can add your PowerShell commands below
